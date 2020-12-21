3dd8c025d739e2fd583f005a43e363c1a17b7091
gummybearr <ehrudghl123@naver.com>
2020-12-21 16:21:12 +0900

    Add line chart

--- graph-gym/src/App.tsx
+++ graph-gym/src/App.tsx
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 import BarChart from './component/BarChart';
+import LineChart from './component/LineChart';
 
 function App() {
   return (
     <div className="App">
       <BarChart/>
+      <LineChart/>
     </div>
   );
 }
